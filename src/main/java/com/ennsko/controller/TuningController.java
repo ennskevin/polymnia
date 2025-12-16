0b835d087ac00de0d89b48662a88a185990440e1
ennskevin <ennskevin@tutanota.com>
2025-12-16 14:18:39 -0700

    More spring setup

--- src/main/java/com/ennsko/controller/TuningController.java
+++ src/main/java/com/ennsko/controller/TuningController.java
@@ -1,5 +1,10 @@
 package com.ennsko.controller;
 
+import com.ennsko.dto.request.TuningRequest;
+import com.ennsko.dto.response.TuningResponse;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,10 @@ public class TuningController {
 
     public TuningController(TuningService tuningService) {
         this.tuningService = tuningService;
+    }
+
+    @PostMapping
+    public TuningResponse run(@RequestBody TuningRequest request) {
+        return null;
     }
 }
---
+++ src/main/java/com/ennsko/dto/response/TuningResponse.java
@@ -0,0 +1,14 @@
+package com.ennsko.dto.response;
+
+import com.ennsko.service.tuning.*;
+import jakarta.validation.constraints.*;
+
+
+public record TuningResponse (
+    @NotNull Harmony harmony,
+    @NotNull TuningSet tuningSet,
+    int referencePos,
+    String tuningType,
+    String calcType,
+    TuningBehavior tuningBehavior
+) {}
