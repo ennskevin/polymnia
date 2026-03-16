0794e9fffe26f09226a4e72ba103c6b8b45ddd18
ennskevin <ennskevin@tutanota.com>
2026-03-16 14:30:00 -0600

    Add polmynia suffix to localhost origin in controller

--- src/main/java/com/ennsko/controller/TuningController.java
+++ src/main/java/com/ennsko/controller/TuningController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ennsko.service.tuning.TuningService;
 
 @CrossOrigin(origins = {
-    "http://localhost:5173",
+    "http://localhost:5173/polymnia",
     "https://ennskevin.com",
     "https://www.ennskevin.com"
 })
